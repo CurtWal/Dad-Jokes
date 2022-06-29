4f889265207c2ba3a50002e36459e3d9ffdcaabf
root <root@DESKTOP-6B3FQL8.localdomain>
2022-06-29 11:28:23 -0500

    added html css and js comments and functions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dad-Jokes
 Generate random dad jokes
+
+with the help of https://engineertodeveloper.com/make-a-dad-joke-generator-with-javascript/
+
+I was able to set this up.
---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+console.log('working');
+
+// grab the joke button from html
+const jokeBtn = document.querySelector(".getaJoke");
+
+// grab the div class named jokes from the html doc
+const jokeDiv = document.querySelector(".jokes");
+
+jokeBtn.addEventListener("click", function(){
+
+    // get the website with the dad jokes to be used
+    fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    // write the dad jokes to the html doc
+    .then(function(data){
+        jokeDiv.textContent = data.joke;
+        console.log(data.joke);
+    })
+});
